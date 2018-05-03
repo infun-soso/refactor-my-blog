f16fda8c69674d01d7168063aefd84ba5bd64c92
infun-soso <973394690@qq.com>
2018-05-03 19:09:38 +0800

    middle vue

---
+++ src/pages/mainpart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main">
+    <el-container>
+      <el-header>
+        <v-header></v-header>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import vHeader from '../components/vHeader'
+export default {
+  name: 'mainpart',
+  components: {
+    'vHeader': vHeader
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
