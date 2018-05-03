147b8a25cb3608a86239134fd7a190c93604b870
infun-soso <973394690@qq.com>
2018-05-03 19:09:00 +0800

    添加分页组件

---
+++ src/components/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pagination">
+    <el-pagination
+      layout="prev, pager, next"
+      :page-size="pageSize"
+      :total="totalItem">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pagination',
+  props: {
+    pageSize: {
+      type: Number,
+      default: 10
+    },
+    totalItem: {
+      type: Number,
+      default: 50
+    }
+  }
+}
+</script>
+
+<style>
+.pagination {
+  text-align: right;
+}
+</style>
